d40b60a4b943af4b8d01d083ce265b461ee44fe5
Jeeho Lee <65565929+jeeheezy@users.noreply.github.com>
2024-02-12 20:14:55 +0000

    Added photo details except forms and working delete links

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -4,4 +4,11 @@ class PhotosController < ApplicationController
     @list_of_photos = matching_photos.order({ :created_at => :desc })
     render({ :template => "photo_templates/list" })
   end
+
+  def show
+    url_id = params["path_id"]
+    matching_relation = Photo.where({ :id => url_id })
+    @photo = matching_relation.first
+    render({ :template => "photo_templates/show" })
+  end
 end
